0edd8f1588f6089d1280f483103c5ae01ba0c90d
sebibin <sebibin@gmail.com>
2023-02-13 18:52:37 +0100

    Added second argument - Output Path

--- KolorTest/Program.cs
+++ KolorTest/Program.cs
@@ -3,7 +3,8 @@ using DeOldifyEngine;
 using System.Reflection;
 using Microsoft.Extensions.Configuration;
 
-
+//################################################################################################################
+// ### PREPARE ###
 var builder = new ConfigurationBuilder()
     .SetBasePath(Directory.GetCurrentDirectory())
     .AddJsonFile("appsettings.json", optional: false);
@@ -35,14 +36,17 @@ if (!modelFiles.Any())
 
 Console.WriteLine($"Model files:");
 Console.WriteLine($"{modelFiles.Aggregate("", (accumulator, piece) => accumulator + "\r\n" + piece)}");
+Console.WriteLine($"");
+//################################################################################################################
 
+//################################################################################################################
+// ### PREPARE ENGINE ###
+//################################################################################################################
 var deOldifyEngine = new DeOldify();  
 
 Console.WriteLine("Initialize DeOldify engine...");
 deOldifyEngine.Initialize(modelsDirectoryPath);
 Console.WriteLine("Initialized.");
-
-var stopWatch = new Stopwatch();
 
 Console.WriteLine("");
 var filesToConvert = Directory.GetFiles(exeDirectoryPath, @"TestFiles/pict*.*");
@@ -56,13 +60,28 @@ else
     Console.WriteLine($"Colorize files in: {exeDirectoryPath}/TestFiles/");
 }
 
+var outputFilesPath = string.Empty;
+if (args.Length > 1 && Directory.Exists(args[1]))
+{
+    outputFilesPath = args[1];
+}
 
+//################################################################################################################
+// ### COLORIZE ###
+//################################################################################################################
+var stopWatch = new Stopwatch();
 foreach (var fileName in filesToConvert)
 {
     var fileInfo = new FileInfo(fileName);
     var fileNameWithoutExt = String.Concat(fileInfo.Name.Take(fileInfo.Name.Length - fileInfo.Extension.Length));
     var newFileName = $"{fileNameWithoutExt}-color{fileInfo.Extension}";
-    var colorFile = Path.Combine(fileInfo.DirectoryName, newFileName);
+    
+    if (string.IsNullOrEmpty(outputFilesPath))
+    {
+        outputFilesPath = fileInfo.DirectoryName;
+    }
+    
+    var colorFile = Path.Combine(outputFilesPath, newFileName);
     
     Console.WriteLine($"Start colorize:\t{DateTime.Now.TimeOfDay:hh\\:mm\\:ss\\:fff} \t\t\t\t\t {fileInfo.Name}");
     
@@ -74,7 +93,9 @@ foreach (var fileName in filesToConvert)
         $"End colorize: \t{DateTime.Now.TimeOfDay:hh\\:mm\\:ss\\:fff} \t Elapsed time:\t{stopWatch.Elapsed:hh\\:mm\\:ss\\:fff} \t {newFileName}");
     Console.WriteLine("");
 }
-
+//################################################################################################################
+// ### COLORIZE ###
+//################################################################################################################
 
 
 
